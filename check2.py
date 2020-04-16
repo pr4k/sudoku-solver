0accc552f08fe254d2d94a04385a1c7d0e27b302
Prakhar <b118038@iiit-bh.ac.in>
2020-04-16 01:33:17 +0000

    Sudoku refined with digits

--- check2.py
+++ check2.py
@@ -1,5 +1,5 @@
 import cv2
-from imutils import contours
+from imutils import contours as cnt_sort
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -94,6 +94,7 @@ size=900
 img = get_sudo('sudoku5.jpg',size)
 thresh = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
                 cv2.THRESH_BINARY_INV,39,10)
+thresh1 = thresh.copy()
 kernel = np.ones((1,1),np.uint8)
 #thresh = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel)
 #thresh = cv2.dilate(thresh,kernel,iterations=3)
@@ -106,13 +107,42 @@ thresh = cv2.morphologyEx(thresh,cv2.MORPH_CLOSE,kernel)
 thresh = cv2.bitwise_not(thresh)
 contours,heirarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 blank = np.zeros(img.shape,np.uint8)
+finalContours = []
 for cnt in contours:
     epsilon = 0.04*cv2.arcLength(cnt,True)
     approx = cv2.approxPolyDP(cnt,epsilon,True)
     approx = cv2.convexHull(cnt)
     area = cv2.contourArea(approx)
     if area <= 9000:
-        blank = cv2.drawContours(blank,[approx],-1,(255,255,255),2)
+        finalContours.append(approx)
+sudoku_rows,_ = cnt_sort.sort_contours(finalContours,method="left-to-right")
+kernel = np.ones((3,3),np.uint8)
+thresh1 = cv2.erode(thresh1,kernel,iterations=1)
+for c in sudoku_rows:
+    blank = cv2.drawContours(blank,[c],-1,(255),-1)
+    blank = cv2.bitwise_and(thresh1,blank,mask=blank)
+
+#print((sudoku_rows))
+#for (i,c) in enumerate(sudoku_rows,1):
+#    blnk = blank
+#    blnk = cv2.drawContours(blnk,[c],-1,(255,255,255),-1)
+#    cv2.imwrite("images/{}.jpg".format(i),blnk)
+kernel = np.ones((5,1),np.uint8)
+blank = cv2.erode(blank,kernel,iterations=1)
+kernel = np.ones((6,6),np.uint8)
+blank = cv2.morphologyEx(blank,cv2.MORPH_CLOSE,kernel)
+kernel = np.ones((1,5),np.uint8)
+blank = cv2.erode(blank,kernel,iterations=1)
+kernel = np.ones((6,6),np.uint8)
+blank = cv2.morphologyEx(blank,cv2.MORPH_CLOSE,kernel)
+kernel = np.ones((6,6),np.uint8)
+blank = cv2.dilate(blank,kernel,iterations=1)
+factor = blank.shape[0]//9
+for i in range(10):
+    print(factor*i)
+    cv2.line(blank,(0,factor*i),(blank.shape[1],factor*i),(255),2,2)
+    cv2.line(blank,(factor*i,0),(factor*i,blank.shape[0]),(255),2,2)
+
 
 plt.subplot(121),plt.imshow(img,cmap='Greys_r')
 plt.subplot(122),plt.imshow(blank,cmap='Greys_r')
